eb24804dce26db7dae90ae0b5123a688f808e6df
Ole Vølund Skov Mortensen <olemortensen8@yahoo.dk>
2018-08-11 21:56:38 +0200

    Initial commit

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -1,17 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace NoteApp
 {
+    public class ConsoleEntries : DbContext {
+        public DbSet<Model.Post> Posts { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            while (true)
+            {
+
+
+                var context = new ConsoleEntries();
+                var post = new Model.Post() {
+                   Title = Console.ReadLine(),
+                   Body = Console.ReadLine(),
+                   DateWritten = DateTime.Now
+                };
+                context.Posts.Add(post);
+                context.SaveChanges();
+                foreach (var entry in context.Posts)
+                {
+                    Console.WriteLine(entry.Title + " " + entry.Body + "\n" + entry.DateWritten);
+                }
+            }
         }
     }
 }
